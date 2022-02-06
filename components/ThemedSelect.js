c86330c6ddd7729422e84c35f931c339f945119b
unknown <lknsaheed@gmail.com>
2022-02-06 18:46:38 +0100

    NEw changes

--- components/ThemedSelect.js
+++ components/ThemedSelect.js
@@ -3,13 +3,22 @@ import Select from "react-select";
 const ThemedSelect = (props) => {
   return (
     <Select
+      theme={(theme) => ({
+        ...theme,
+        colors: {
+          ...theme.colors,
+          primary25: "#8800ff25",
+          primary: "#8800ff",
+        },
+      })}
       styles={{
         control: (base, state) => ({
           ...base,
-          boxShadow: state.isFocused ? 0 : "initial",
+          minHeight: "43px",
+          boxShadow: state.isFocused ? "0 0 0 1px #8800ff" : "none",
           "&:hover": {
-            border: state.isFocused ? 0 : 0,
-            boxShadow: state.isFocused ? 0 : 0,
+            borderColor: state.isFocused ? "#8800ff" : "#a7a7a7",
+            boxShadow: state.isFocused ? "0 0 0 1px #8800ff" : "none",
           },
         }),
       }}
